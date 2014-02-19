a785aa96c127c4ffdeaaa2daff4a1ddf2cb20d5c
prontokan <kan@prontomarketing.com>
2014-02-19 20:49:20 +0700

    refactor test a bit

--- roman/roman_tests.py
+++ roman/roman_tests.py
@@ -4,27 +4,26 @@ from roman import Roman
 
 
 class RomanTests(unittest.TestCase):
+    def setUp(self):
+        self.roman = Roman()
+
     def test_input_1_should_return_I(self):
-        roman = Roman()
-        result = roman.convert(1)
+        result = self.roman.convert(1)
 
         self.assertEqual('I', result)
 
     def test_input_2_should_return_II(self):
-        roman = Roman()
-        result = roman.convert(2)
+        result = self.roman.convert(2)
 
         self.assertEqual('II', result)
 
     def test_input_3_should_return_III(self):
-        roman = Roman()
-        result = roman.convert(3)
+        result = self.roman.convert(3)
 
         self.assertEqual('III', result)
 
     def test_input_4_should_return_IV(self):
-        roman = Roman()
-        result = roman.convert(4)
+        result = self.roman.convert(4)
 
         self.assertEqual('IV', result)
 
