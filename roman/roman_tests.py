ddae653b33ca72c2f59d8aef3fcc80c8ffd2aefd
prontokan <kan@prontomarketing.com>
2014-02-19 21:20:28 +0700

    move expected value to be the second argument

--- roman/roman_tests.py
+++ roman/roman_tests.py
@@ -10,52 +10,52 @@ class RomanTests(unittest.TestCase):
     def test_input_1_should_return_I(self):
         result = self.roman.convert(1)
 
-        self.assertEqual('I', result)
+        self.assertEqual(result, 'I')
 
     def test_input_2_should_return_II(self):
         result = self.roman.convert(2)
 
-        self.assertEqual('II', result)
+        self.assertEqual(result, 'II')
 
     def test_input_3_should_return_III(self):
         result = self.roman.convert(3)
 
-        self.assertEqual('III', result)
+        self.assertEqual(result, 'III')
 
     def test_input_4_should_return_IV(self):
         result = self.roman.convert(4)
 
-        self.assertEqual('IV', result)
+        self.assertEqual(result, 'IV')
 
     def test_input_5_should_return_V(self):
         result = self.roman.convert(5)
 
-        self.assertEqual('V', result)
+        self.assertEqual(result, 'V')
 
     def test_input_6_should_return_VI(self):
         result = self.roman.convert(6)
 
-        self.assertEqual('VI', result)
+        self.assertEqual(result, 'VI')
 
     def test_input_7_should_return_VII(self):
         result = self.roman.convert(7)
 
-        self.assertEqual('VII', result)
+        self.assertEqual(result, 'VII')
 
     def test_input_8_should_return_VIII(self):
         result = self.roman.convert(8)
 
-        self.assertEqual('VIII', result)
+        self.assertEqual(result, 'VIII')
 
     def test_input_9_should_return_IX(self):
         result = self.roman.convert(9)
 
-        self.assertEqual('IX', result)
+        self.assertEqual(result, 'IX')
 
     def test_input_10_should_return_X(self):
         result = self.roman.convert(10)
 
-        self.assertEqual('X', result)
+        self.assertEqual(result, 'X')
 
 
 if __name__ == '__main__':
