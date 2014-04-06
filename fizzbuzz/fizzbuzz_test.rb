9e25aacc61f291f62a1d5567a20ffc6a9b3baf68
prontokan <kan@prontomarketing.com>
2014-04-06 07:33:18 +0700

    handle case input is 1

--- fizzbuzz/fizzbuzz_test.rb
+++ fizzbuzz/fizzbuzz_test.rb
@@ -1,10 +1,20 @@
 class FizzBuzz
     def take(num)
-        'Fizz'
+        if num == 1
+            1
+        else
+            'Fizz'
+        end
     end
 end
 
 describe FizzBuzz do
+    it "returns 1 if input 1" do
+        fizzbuzz = FizzBuzz.new
+        result = fizzbuzz.take(1)
+        result.should eq(1)
+    end
+
     it "returns Fizz if input 3" do
         fizzbuzz = FizzBuzz.new
         result = fizzbuzz.take(3)
