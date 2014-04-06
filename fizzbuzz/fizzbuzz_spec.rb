f1931729f903d9570f4e8f5203f8fd37a54d3dfa
prontokan <kan@prontomarketing.com>
2014-04-06 07:50:31 +0700

    change should to expect (new style)

--- fizzbuzz/fizzbuzz_spec.rb
+++ fizzbuzz/fizzbuzz_spec.rb
@@ -4,55 +4,54 @@ describe FizzBuzz do
     it "returns 1 if input 1" do
         fizzbuzz = FizzBuzz.new
         result = fizzbuzz.take(1)
-        result.should eq(1)
+        expect(result).to eq(1)
     end
 
     it "returns 2 if input 2" do
         fizzbuzz = FizzBuzz.new
         result = fizzbuzz.take(2)
-        result.should eq(2)
+        expect(result).to eq(2)
     end
 
     it "returns Fizz if input 3" do
         fizzbuzz = FizzBuzz.new
         result = fizzbuzz.take(3)
-        result.should eq("Fizz")
+        expect(result).to eq("Fizz")
     end
 
     it "returns 4 if input 4" do
         fizzbuzz = FizzBuzz.new
         result = fizzbuzz.take(4)
-        result.should eq(4)
+        expect(result).to eq(4)
     end
 
     it "returns Buzz if input 5" do
         fizzbuzz = FizzBuzz.new
         result = fizzbuzz.take(5)
-        result.should eq("Buzz")
+        expect(result).to eq("Buzz")
     end
 
     it "returns Fizz if input 6" do
         fizzbuzz = FizzBuzz.new
         result = fizzbuzz.take(6)
-        result.should eq("Fizz")
+        expect(result).to eq("Fizz")
     end
 
     it "returns Buzz if input 10" do
         fizzbuzz = FizzBuzz.new
         result = fizzbuzz.take(10)
-        result.should eq("Buzz")
+        expect(result).to eq("Buzz")
     end
 
     it "returns FizzBuzz if input 15" do
         fizzbuzz = FizzBuzz.new
         result = fizzbuzz.take(15)
-        result.should eq("FizzBuzz")
+        expect(result).to eq("FizzBuzz")
     end
 
     it "returns FizzBuzz if input 30" do
         fizzbuzz = FizzBuzz.new
         result = fizzbuzz.take(30)
-        result.should eq("FizzBuzz")
+        expect(result).to eq("FizzBuzz")
     end
 end
-
