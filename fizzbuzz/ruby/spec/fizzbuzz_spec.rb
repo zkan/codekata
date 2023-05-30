4bf1d5545441419bea7d45fe194c18a914eb1c91
zkan <zkan.cs@gmail.com>
2023-05-30 07:52:11 +0700

    feat: Say Fizz when input is 3

--- fizzbuzz/ruby/spec/fizzbuzz_spec.rb
+++ fizzbuzz/ruby/spec/fizzbuzz_spec.rb
@@ -1,11 +1,17 @@
 require "./src/fizzbuzz.rb"
 
 RSpec.describe FizzBuzz do
-  let(:fizzbuzz) { described_class.new(1) }
+  let(:fizzbuzz) { described_class.new(3) }
 
   describe "initializing" do
     it "creates a new instance" do
       expect(fizzbuzz).to be_instance_of(FizzBuzz)
+    end
+  end
+
+  describe "Fizz" do
+    it "says Fizz when input is 3" do
+      expect(fizzbuzz.say()).to eq("Fizz")
     end
   end
 end
--- fizzbuzz/ruby/src/fizzbuzz.rb
+++ fizzbuzz/ruby/src/fizzbuzz.rb
@@ -2,4 +2,10 @@ class FizzBuzz
   def initialize(n = 1)
     @number = n
   end
+
+  def say()
+    if @number == 3
+      "Fizz"
+    end
+  end
 end
